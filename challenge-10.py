a09227ff4d146c587f6a52cf15d33b661eecc45a
syacia09 <keisya.valencia03@gmail.com>
2024-02-23 19:59:20 +0700

    Edit

--- challenge-10.py
+++
@@ -1,3 +0,0 @@
-list = ["Do", "Re", "Mi", "Fa", "So", "La", "Ti"]
-list2 = list[2:3] + list[0:1] + list[3:7] + list[1:2]
-print(f"{list2}")
--- challenge-9.py
+++ challenge-9.py
@@ -1,3 +1,9 @@
+print("<<<\t\tLatihan 1\t\t>>>")
+print("Berikut akan tampil list dengan kondisi : ")
+print("1. Range(1, 100)")
+print("2. Value/Item : genap (i >= 10 dan i <= 20)")
+print("3. Value/Item : ganjil (i >= 90 dan i <= 100)/n")
+print()
 list_num = []
 for i in range(100) :
     i += 1
@@ -8,3 +14,17 @@ for i in range(100) :
         if (i >= 90 and i <= 100) :
             list_num.append(i)
 print(f"{list_num}")
+print()
+print("<<<\t\tLatihan 2\t\t>>>")
+list = ["Do", "Re", "Mi", "Fa", "So", "La", "Ti"]
+list2 = list[2:3] + list[0:1] + list[3:7] + list[1:2]
+print(f"{list2}")
+print()
+print("<<<\t\tLatihan 3\t\t>>>")
+data = "IgNatIus"
+list = []
+len = len(data)
+for i in range (len) :
+    if data[i].isupper() :
+        list.append(data[i])
+print(f"{list}")
